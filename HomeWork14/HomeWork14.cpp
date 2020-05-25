2e8b97464c3080f8d316003c0a18f59116b568a0
Artur <arturrabbit3@gmail.com>
2020-05-26 01:11:02 +0300

    Добавьте файлы проекта.

---
+++ HomeWork14/HomeWork14.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string str("Hello, World!");
+    int strLength = str.length();
+    char fristSymbol = str.at(0), lastSymbol = str.at(strLength - 1);
+    std::cout << "currents string is: " << str << " String length is: " << strLength
+        << ". First symbol is: " << fristSymbol << ". Last symbol is: " << lastSymbol;
+}
